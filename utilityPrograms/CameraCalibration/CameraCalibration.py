7a9442bc8e96baf3842120cadac0edfd1a6ca127
Ed Whitwell <edwhitwell1507@gmail.com>
2025-06-29 13:44:54 +0100

    stereo calibration

--- utilityPrograms/CameraCalibration/CameraCalibration.py
+++ utilityPrograms/CameraCalibration/CameraCalibration.py
@@ -7,6 +7,7 @@ imageiterator = 14
 grid = (7,7)
 
 cam = Camera(0)
+rightCam = Camera(1)
 
 
 
@@ -22,19 +23,28 @@ while (running):
 	frame = None
 	while not cornersFound:
 		frame = cam.get_frame()
+		rightFrame = rightCam.get_frame()
 		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+		rightGray = cv.cvtColor(rightFrame, cv.COLOR_BGR2GRAY)
 		print("what")
 		cornersFound, corners = cv.findChessboardCorners(gray, grid, None)	
+
 		if cornersFound:
 			visualise = frame.copy()
 			cv.drawChessboardCorners(visualise, grid, corners, cornersFound)
 			cv.waitKey(3000)
-			print("howdy")
+			print("howdy")		
+			cornersFoundRight, cornersRight = cv.findChessboardCorners(rightGray, grid, None)
+			if cornersFoundRight:
+				visualiseRight = rightFrame.copy()
+				cv.drawChessboardCorners(visualiseRight, grid, cornersRight, cornersFoundRight)
+				cv.imshow("visualiseRight", visualiseRight)
+				cv.waitKey(3000)
 		else:
 			cv.imshow("visualise", cam.get_frame())
 			cv.waitKey(3000)
-	
-	cv.imwrite("CalIm" + str(imageiterator) + ".jpg", frame)
-	
+
+	cv.imwrite("CalImLeft" + str(imageiterator) + ".jpg", frame)
+	cv.imwrite("CalImRight" + str(imageiterator) + ".jpg", rightFrame)
 	imageiterator = imageiterator + 1
 
