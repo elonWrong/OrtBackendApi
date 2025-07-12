3fb32412c340b4af81836492b3eab6b80300fef4
EdWhitwell <edwhitwell1507@gmail.com>
2025-07-12 16:10:03 +0100

    imu updates

--- src/imu.py
+++ src/imu.py
@@ -12,10 +12,22 @@ class IMU:
 	ACCEL_X_REGISTER = 6
 	ACCEL_Y_REGISTER = 7
 	ACCEL_Z_REGISTER = 8
+	
+	SAMPLE_RATE = 0.01  # 10 ms sample rate
 
 	def __init__(self):
 		self.bus = smbus2.SMBus(1)
-		self.resetSpeed()
+		self.previousReading = None
+		self.speed = {
+			"x": 0, 
+			"y": 0,
+			"z": 0
+		}
+		self.displacement = {
+			"x": 0, 
+			"y": 0,
+			"z": 0
+		}
 
 	def resetSpeed(self):
 		self.speed = {
@@ -23,22 +35,38 @@ class IMU:
 			"y": 0,
 			"z": 0
 		}
+	def resetDisplacement(self):
+		self.displacement = {
+			"x": 0, 
+			"y": 0,
+			"z": 0
+		}
 
 	def generateStream(self):
 		while True:
+			timeStamp = time.time()
 			orientation = self.getOrientation()
 			acceleration = self.getAcc()
+			self.updateSpeed(acceleration)
+			self.updateDisplacement(timeStamp - (self.previousReading["timeStamp"] if self.previousReading else timeStamp))
 			if orientation and acceleration:
 				data = {
 					"orientation": orientation,
-					"acceleration": acceleration
+					"acceleration": acceleration,
+					"speed": self.speed,
+					"displacement": self.displacement,
+					"timeStamp": timeStamp,
+					"timeElapsed": timeStamp - self.previousReading["timeStamp"] if self.previousReading else 0,
+					"previousOrientation": self.previousReading["orientation"] if self.previousReading else None,
+					"previousAcceleration": self.previousReading["acceleration"] if self.previousReading else None
 				}
+				self.previousReading = timeStamp
 				yield json.dumps(data) + "\n"
 			elif orientation is None:
 				yield json.dumps({"error": "Failed to read orientation"}) + "\n"
 			elif acceleration is None:
 				yield json.dumps({"error": "Failed to read acceleration"}) + "\n"
-			time.sleep(0.1)
+			time.sleep(self.SAMPLE_RATE)
 
 	def i2cRead(self, registerAddress, numBytes=1):
 		return self.bus.read_i2c_block_data(self.I2C_ADDRESS, registerAddress, numBytes)
@@ -80,12 +108,28 @@ class IMU:
 			acc_y = to_signed16(acc_bytes[2], acc_bytes[3])
 			acc_z = to_signed16(acc_bytes[4], acc_bytes[5])
 
-			return {
-				"x": acc_x,
+			acc = {
+				"x": acc_x,  # Convert to g
 				"y": acc_y,
 				"z": acc_z
 			}
+
+			return acc
 		except Exception as e:
 			print("I2C read error (acceleration):", e)
 			return None
+		
+	def updateSpeed(self, acc):
+		if acc:
+			self.speed["x"] += acc["x"] * self.SAMPLE_RATE
+			self.speed["y"] += acc["y"] * self.SAMPLE_RATE	
+			self.speed["z"] += acc["z"] * self.SAMPLE_RATE
+
+	def updateDisplacement(self, timeElapsed):
+		print("moved by", self.speed, "in", timeElapsed, "seconds")
+		print("displacement before", self.displacement)
+		self.displacement["x"] += self.speed["x"] * timeElapsed
+		self.displacement["y"] += self.speed["y"] * timeElapsed
+		self.displacement["z"] += self.speed["z"] * timeElapsed
+		print("displacement after", self.displacement)
 
